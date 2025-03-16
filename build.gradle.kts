6192b6bcfc3c983e74e5c5cc46219d9651862a6e
Arrafly Dori <arrafly.ad@gmail.com>
2025-03-16 15:09:47 +0700

    build: update compileSdk to 34, use version catalog, add navigation and serialization deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@
 plugins {
     id("com.android.application") version "8.9.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10" apply false
 }
