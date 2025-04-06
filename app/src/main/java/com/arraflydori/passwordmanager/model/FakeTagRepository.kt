ab506cd7cfb1990beba277391d0555ed6ee66be0
Arrafly Dori <arrafly.ad@gmail.com>
2025-04-06 19:48:53 +0700

    fix: randomize dummy Tag generation for each Vault

--- app/src/main/java/com/arraflydori/passwordmanager/model/FakeTagRepository.kt
+++ app/src/main/java/com/arraflydori/passwordmanager/model/FakeTagRepository.kt
@@ -1,17 +1,38 @@
 package com.arraflydori.passwordmanager.model
 
+import kotlin.random.Random
+
 class FakeTagRepository : TagRepository {
     private val tagMap = mutableMapOf<String, MutableSet<String>>()
     private val dummyTags = setOf(
-        "Service", "Finance", "Education", "Government", "Tech", "Social", "Lifestyle", "Entertainment"
+        "Service",
+        "Finance",
+        "Education",
+        "Government",
+        "Tech",
+        "Social",
+        "Lifestyle",
+        "Entertainment"
     )
 
     override fun getTags(vaultId: String): Set<String> {
-        return tagMap.getOrPut(vaultId) { dummyTags.toMutableSet() }
+        return tagMap.getOrPut(vaultId) {
+            mutableSetOf<String>().apply {
+                repeat(Random.nextInt(1, dummyTags.size)) {
+                    add(dummyTags.random())
+                }
+            }
+        }
     }
 
     override fun createTag(vaultId: String, tag: String): Boolean {
-        val tags = tagMap.getOrPut(vaultId) { mutableSetOf() }
+        val tags = tagMap.getOrPut(vaultId) {
+            mutableSetOf<String>().apply {
+                repeat(Random.nextInt(1, dummyTags.size)) {
+                    add(dummyTags.random())
+                }
+            }
+        }
         return tags.add(tag)
     }
 
